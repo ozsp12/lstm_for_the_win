4a50d1e8a85f181b6fb12b749e1dba3fcc5ef456
Osvaldo L Santos-Pereira <54546141+ozsp12@users.noreply.github.com>
2026-08-15 09:36:37 -0300

    chore: remove unused dashboard package

--- src/lstm_for_the_win/dashboard/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Streamlit dashboard support functions."""
-
-from .data import RunBundle, discover_runs, filter_predictions, load_run
-
-__all__ = ["RunBundle", "discover_runs", "filter_predictions", "load_run"]
